7afbbf8ebac804c140caa48bb5547af870829a68
Walter Huf <hufman@gmail.com>
2024-01-10 07:43:09 -0800

    Close existing Etch connections when Flutter app restarts

--- android/src/main/kotlin/io/bimmergestalt/headunit/CarServer.kt
+++ android/src/main/kotlin/io/bimmergestalt/headunit/CarServer.kt
@@ -12,6 +12,7 @@ import java.io.IOException
 
 class CarServer(val host: String = "0.0.0.0", val port: Int = 4006, val amManager: AMManager, val rhmiManager: RHMIManager) {
 	private val uri = "tcp://$host:$port?Packetizer.maxPktSize=8388608&TcpTransport.noDelay=true"
+	private var listener: BMWRemotingListener? = null
 	private var serverFactory: ServerFactory? = null
 
 	fun startServer() {
@@ -19,7 +20,9 @@ class CarServer(val host: String = "0.0.0.0", val port: Int = 4006, val amManage
 			stopServer()
 		}
 		try {
-			val serverFactory = BMWRemotingHelper.newListener(uri, null, BMWRemotingListener(amManager, rhmiManager))
+			val listener = BMWRemotingListener(amManager, rhmiManager)
+			this.listener = listener
+			val serverFactory = BMWRemotingHelper.newListener(uri, null, listener)
 			serverFactory.transportControl(Transport.START, 4000)
 			this.serverFactory = serverFactory
 		} catch (e: Exception) {
@@ -28,6 +31,8 @@ class CarServer(val host: String = "0.0.0.0", val port: Int = 4006, val amManage
 	}
 
 	fun stopServer() {
+		listener?.shutdown()
+		listener = null
 		serverFactory?.transportControl(Transport.STOP_AND_WAIT_DOWN, 4000)
 		serverFactory?.transportControl(Transport.RESET, 4000)
 		serverFactory = null
@@ -35,9 +40,23 @@ class CarServer(val host: String = "0.0.0.0", val port: Int = 4006, val amManage
 
 	class BMWRemotingListener(val amManager: AMManager, val rhmiManager: RHMIManager): BMWRemotingHelper.BMWRemotingServerFactory {
 		/** Listens for new Etch connections */
+		private val connections = ArrayList<RemoteBMWRemotingClient?>()
 
 		override fun newBMWRemotingServer(client: RemoteBMWRemotingClient?): BMWRemotingServer {
-			return BMWRemotingServerImpl(client!!, amManager, rhmiManager);
+			connections.add(client)
+			val server = BMWRemotingServerImpl(client!!, amManager, rhmiManager)
+			return server
+		}
+
+		fun shutdown() {
+			connections.forEach {
+				try {
+					it?._stopAndWaitDown(2000)
+				} catch (e: IllegalStateException) {
+					// already shut down
+//					Log.e("BMWRemoting", "Exception while stopping $it", e)
+				}
+			}
 		}
 	}
 }
